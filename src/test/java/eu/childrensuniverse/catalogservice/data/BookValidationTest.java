ba90c5b8b74322c952554cba67c3e5077c164f9d
Petre Vane <petre.vane@aws-dev.cloud>
2024-05-13 22:33:53 +0200

    Added validation test

---
+++ src/test/java/eu/childrensuniverse/catalogservice/data/BookValidationTest.java
@@ -0,0 +1,44 @@
+package eu.childrensuniverse.catalogservice.data;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class BookValidationTest {
+
+    public static Validator validator;
+
+    @BeforeAll
+    public static void setUp() {
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+
+    @Test
+    public void successValidation()
+    {
+        Book book = new Book("1234567890", "Title", "Author", 9.90);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    public void failedValidation()
+    {
+        Book book = new Book("123456dsfgf7890", "Title", "Author", 9.90);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertFalse(violations.isEmpty());
+        assertThat(violations.iterator().next().getMessage()).isEqualTo("The ISBN format must be valid.");
+    }
+
+}
